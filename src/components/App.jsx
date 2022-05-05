71840f2cb4c0d6f46563099444c55b13a6992e28
Yura Dolzhnikov <yuradolzhnikov@gmail.com>
2022-05-05 17:43:14 +0300

    Fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
-import { lazy, Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Routes } from "react-router-dom";
 import AppBar from "./AppBar";
 import Container from "./Container";
 
--- src/services/API.js
+++
@@ -1,3 +0,0 @@
-const API_KEY = 'ba05e54d26cce7ef6afa7bb1f3502c89';
-const BASE_URL = 'https://api.themoviedb.org/3';
-

ffbdbcfedf77323fa4e6836ad5650ddbf2f479e6
Yuradv <86840604+Yuradv@users.noreply.github.com>
2022-05-08 14:54:36 +0000

    Deploying to gh-pages from @ Yuradv/goit-react-hw-05-movies@01abc420ab19cb37a92c44985b353ecb011113db 🚀

---
+++ static/js/650.31b86301.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[650],{4650:function(e,i,t){t.r(i),t.d(i,{default:function(){return s}});var n=t(3545),r="Spinner_loader__GQ3jW",c=t(184),s=function(){return(0,c.jsx)("div",{className:r,children:(0,c.jsx)(n.iT,{color:"#00BFFF",height:80,width:80})})}}}]);
+//# sourceMappingURL=650.31b86301.chunk.js.map
